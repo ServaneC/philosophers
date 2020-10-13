154d176a19f1bda010b477acd64d50156866d6ca
ServaneC <schene@student.42.fr>
2020-10-13 12:28:47 +0200

    first

---
+++ philo_one/srcs/main.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: schene <schene@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/10/13 10:47:14 by schene            #+#    #+#             */
+/*   Updated: 2020/10/13 12:26:20 by schene           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo_one.h"
+
+int		main()
+{
+	struct timeval start_time;
+	struct timeval end_time;
+
+	gettimeofday(&start_time, NULL);
+	usleep(100);	
+	gettimeofday(&end_time, NULL);
+	printf("seconds : %ld,%ld\n\n",
+    start_time.tv_sec, start_time.tv_usec);
+	printf("seconds : %ld,%ld\n",
+    end_time.tv_sec, end_time.tv_usec);
+	return (0);
+}
---
+++ philo_one/srcs/philo_one.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo_one.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: schene <schene@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/10/13 11:35:57 by schene            #+#    #+#             */
+/*   Updated: 2020/10/13 12:22:59 by schene           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_ONE_H
+# define PHILO_ONE_H
+
+# include <unistd.h>
+# include <sys/time.h>
+# include <stdio.h>
+
+void	ft_putstr(char *str);
+
+#endif
