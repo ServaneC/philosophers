e064768ef3f51293cdca6bdb3c1babbb6fa60822
ServaneC <schene@student.42.fr>
2020-10-23 11:38:47 +0200

    p2 death test

--- philo_two/srcs/philo_life.c
+++ philo_two/srcs/philo_life.c
@@ -6,7 +6,7 @@
 /*   By: schene <schene@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/16 12:13:37 by schene            #+#    #+#             */
-/*   Updated: 2020/10/22 15:19:24 by schene           ###   ########.fr       */
+/*   Updated: 2020/10/23 11:35:35 by schene           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,9 @@
 
 static void		taking_forks(t_id *id)
 {
-	while (forks < 2)
-		;
-	forks -= 1;
+	forks -= 2;
 	sem_wait(id->data->sem);
 	print_state(id, TAKE_FORK);
-	forks -= 1;
 	sem_wait(id->data->sem);
 	print_state(id, TAKE_FORK);
 }
@@ -49,11 +46,14 @@ void			*philo_life(void *arg)
 	last_meal = get_time_ms();
 	while (timestamp_ms(last_meal) < id->data->time_die && !(g_death))
 	{
-		taking_forks(id);
-		last_meal = philo_eat(id);
-		if (++id->nb_meals == id->data->must_eat)
-			return (NULL);
-		print_state(id, THINK);
+		if (forks >= 2)
+		{
+			taking_forks(id);
+			last_meal = philo_eat(id);
+			if (++id->nb_meals == id->data->must_eat)
+				return (NULL);
+			print_state(id, THINK);
+		}
 	}
 	g_death++;
 	return (print_state(id, DEAD));
